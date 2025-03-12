313c73d7967d077ba65fe5646aefa0e2d329683e
lequocanhlhu122001209 <quocanhpentakill@gmail.com>
2025-03-12 11:09:49 +0700

    HSTSLCVN_thanhnavigation_Password-is-type_Validation-login-form_Submit-form

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './src/screens/LoginScreen';
-
+import LoginScreen from './src/screens/LoginScreen'; // Đường dẫn đến LoginScreen.js
+import MainScreen from './src/screens/MainScreen';   // Đường dẫn đến MainScreen.js
+// cài thư viện thêm = code : npm install @react-navigation/native @react-navigation/stack react-native-screens react-native-safe-area-context react-native-gesture-handler react-native-reanimated react-native-vector-icons
+// cài thêm thư viện npm install react-native-chart-kit
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Main" 
+          component={MainScreen} 
+          options={{ headerShown: false }} // Ẩn header cho MainScreen nếu không cần
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-// cài thư viện thêm = code : npm install @react-navigation/native @react-navigation/stack react-native-screens react-native-safe-area-context react-native-gesture-handler react-native-reanimated react-native-vector-icons
+
